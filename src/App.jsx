9467222b82ee8fccdb0059dcf8abd17a4e7c9625
Seda Akin <dalyanogluseda@gmail.com>
2025-04-25 23:34:55 +0300

    first react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-
+import { useState } from "react";
 
 function App() {
-  return <h1>Hello world!</h1>;
+    const [state, setState] = useState("bu bir javascript eğitimidir");
+
+    function handleClick() {
+        setState("bu bir react eğitimidir")
+    }
+
+  return (<div>
+    <h1>{state}</h1>
+    <button onClick={handleClick}>değiştir</button>
+  </div>
+  );
 }
 
 export default App;
