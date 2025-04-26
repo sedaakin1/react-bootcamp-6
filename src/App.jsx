8d3e3d197f9e88cdb95c9f2040b0e06c034635e3
Seda Akin <dalyanogluseda@gmail.com>
2025-04-26 21:40:57 +0300

    product item component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
+import ProductItem from "./ProductItem";
 
 function App() {
-    const [state, setState] = useState("bu bir javascript eğitimidir");
-
-    function handleClick() {
-        setState("bu bir react eğitimidir")
-    }
-
-  return (<div>
-    <h1>{state}</h1>
-    <button onClick={handleClick}>değiştir</button>
-  </div>
+    
+  return (
+    <div>
+      <ProductItem />
+    </div>
   );
 }
 
---
+++ src/ProductItem.jsx
@@ -0,0 +1,19 @@
+function ProductItem(){
+
+    return(
+        <div className="product-item">
+            <div className="product-image">
+               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRozpqAoVcladM2kWA3HfxRwgbfc81uHZqmzg&s" 
+               alt="Soda"/>
+            </div>
+            <div className="product-info">
+               <b className="product-title">Soda</b>
+               <span className="product-price">4$</span>
+            </div>
+        </div>
+        
+    )
+
+}
+
+export default ProductItem
