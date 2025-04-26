753bc94ce0543d7a55330f47de75c6ad63fe8cf1
Seda Akin <dalyanogluseda@gmail.com>
2025-04-26 22:05:00 +0300

    product item component css

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,10 +1,16 @@
+import "./ProductItem.css";
+
 function ProductItem(){
 
     return(
-        <div className="product-item">
+        <div className="product-item" 
+        style={{ }}>
             <div className="product-image">
                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRozpqAoVcladM2kWA3HfxRwgbfc81uHZqmzg&s" 
-               alt="Soda"/>
+               alt="Soda"
+                style={{ }}
+               />
+               
             </div>
             <div className="product-info">
                <b className="product-title">Soda</b>
