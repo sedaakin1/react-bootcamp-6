35cf8870f6088186fbc39fd9c729c4a3ede31cc4
Seda Akin <dalyanogluseda@gmail.com>
2025-05-03 22:12:12 +0300

    tek state ve tek function ile yönetim

--- src/components/Products/AddProduct.jsx
+++ src/components/Products/AddProduct.jsx
@@ -5,35 +5,48 @@ import React, { useState } from "react";
 
 const AddProduct = () => {
 
-    const [title, setTitle] = useState("");
-    const [price, setPrice] = useState(0);
-    const [imageUrl, setImageUrl] = useState("");
+    const [product, setProduct] = useState({
 
-    function handleTitleChange(event){
-        setTitle(event.target.value)
-    }
-    function handlePriceChange(event){
-        setTitle(event.target.value)
-    }function handleImageChange(event){
-        setTitle(event.target.value)
-    }
+        title:"",
+        price: "",
+        imageUrl: "",
+    })
+
+    function handleChange({target: {name, value}}){
+        setProduct({...product,[name]: value })
+    }       
+
+
+    // const [title, setTitle] = useState("");
+    // const [price, setPrice] = useState(0);
+    // const [imageUrl, setImageUrl] = useState("");
+
+    // function handleTitleChange(event){
+    //     setTitle(event.target.value)
+    // }
+    // function handlePriceChange(event){
+    //     setPrice(event.target.value)
+    // }
+    // function handleImageChange(event){
+    //     setImageUrl(event.target.value)
+    // }
 
   return (
     <form className='add-product-form'>
-    {title} <br/>
-    {price} <br/>
-    {imageUrl} <br/>
+    {product.title} <br/>
+    {product.price} <br/>
+    {product.imageUrl} <br/>
     <label>
         Title:
-        <input type="text" name="title" onChange={handleTitleChange}/>
+        <input type="text" name="title" onChange={handleChange}/>
     </label>
     <label>
         Title:
-        <input type="number" name="price" onChange={handlePriceChange}/>
+        <input type="number" name="price" onChange={handleChange}/>
     </label>
     <label>
         Title:
-        <input type="text" name="imageUrl" onChange={handleImageChange}/>
+        <input type="text" name="imageUrl" onChange={handleChange}/>
     </label>
     <Button color="success">Yeni Ürün Ekle</Button>
 
--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,31 +1,29 @@
 import { useState } from "react";
- import "./ProductItem.css";
- import Button from "../UI/Button";
- 
- function ProductItem(props) {
-   const { image, globalTitle, setGlobalTitle, title, price } = props;
-   const { image, title, price } = props;
-   const [localTitleState, setTitleLocalState] = useState(title);
- 
-   function handleTitleChange() {
-     setTitleLocalState("Yüzük");
-   }
- 
-   return (
-     <div className="product-item">
-       <div className="product-image">
-         <img src={image} alt="Soda" />
-       </div>
-       <div className="product-info">
-         <b className="product-title">{localTitleState}</b>
-         <span className="product-price">{price}₺</span>
-         <Button color="primary" size="sm" onClick={handleTitleChange}>Title Değiştir!</Button>
-         <Button color="primary" size="sm" onClick={handleTitleChange}>
-           Title Değiştir!
-         </Button>
-       </div>
-     </div>
-   );
- }
- 
- export default ProductItem;
+import "./ProductItem.css";
+import Button from "../UI/Button";
+
+function ProductItem(props) {
+  const { image, title, price } = props;
+  const [localTitleState, setTitleLocalState] = useState(title);
+
+  function handleTitleChange() {
+    setTitleLocalState("Yüzük");
+  }
+
+  return (
+    <div className="product-item">
+      <div className="product-image">
+        <img src={image} alt="Soda" />
+      </div>
+      <div className="product-info">
+        <b className="product-title">{localTitleState}</b>
+        <span className="product-price">{price}₺</span>
+        <Button color="primary" size="sm" onClick={handleTitleChange}>
+          Title Değiştir!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductItem;
