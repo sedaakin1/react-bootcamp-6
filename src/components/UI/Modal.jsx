a148597bcef027264b60cb38a106ef2a9b258f64
Seda Akin <dalyanogluseda@gmail.com>
2025-05-16 21:46:26 +0300

    use effect hook clean-up function

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,16 +1,39 @@
 import { createPortal } from "react-dom";
 import "./Modal.css";
+import { useEffect, useState } from "react";
 
 const Modal = ({ setIsShowModal, title, desc }) => {
+   const [count, setCount] = useState(0);
+
   function handleModelClose() {
     setIsShowModal(false);
   }
+
+  useEffect(() => {
+    console.log("component DOM'a ilk yüklendiğinde çalıştı!");
+    let i = 0;
+
+    const id = setInterval(() => {
+      i += 1;
+      setCount(i);
+      console.log(i);
+    }, 1000);
+
+    // clean-up function
+    return () => {
+      console.log("component DOM'dan kaldırıldığında çalıştı!");
+      clearInterval(id);
+    };
+  }, []);
+
   return createPortal(
     <div className="modal">
       <div className="modal-overlay" onClick={handleModelClose}></div>
       <div className="modal-content">
         <div className="modal-header">
           <h5 className="modal-title">{title}</h5>
+          <h5 className="modal-title">{count}</h5>
+
            <span className="close-button" onClick={handleModelClose}>
             &times;
           </span>
